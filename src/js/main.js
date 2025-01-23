49b489f411a39370f1cfb5ede15f18dc15c94ca6
justinemauron <justine.mauron@studentfr.ch>
2025-01-23 09:48:31 +0100

    ex-javascript-1

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const box = document.querySelector(".box.change-color");
+const changeColor = document.querySelectorAll(".box")[0];
+const toggleColor = document.querySelectorAll(".box")[1];
+const trigger = document.querySelectorAll(".box")[2];
+const target = document.querySelectorAll(".box")[3];
+const triggerAll = document.querySelectorAll(".box")[4];
+const triggerAllSelf = document.querySelectorAll(".box")[5];
 
-  if (box) {
-    box.addEventListener("click", () => {
-      box.style.background = "blue";
-    });
+changeColor.addEventListener("click", () => {
+  changeColor.style.backgroundColor = "blue";
+});
+
+toggleColor.addEventListener("click", () => {
+  if (toggleColor.style.backgroundColor === "red") {
+    toggleColor.style.backgroundColor = "orange";
+  } else {
+    toggleColor.style.backgroundColor = "red";
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const box = document.querySelector(".box.toggle-color");
-
-  if (box) {
-    let isRed = false;
-
-    box.addEventListener("click", () => {
-      if (isRed) {
-        box.style.background = "orange";
-      } else {
-        box.style.background = "red";
-      }
-      isRed = !isRed;
-    });
+trigger.addEventListener("click", () => {
+  if ((target.style.display = "block")) {
+    target.style.display = "none";
+  } else {
+    target.style.display = "block";
   }
+});
+
+toggleColor.addEventListener("click", () => {
+  target.style.display = "block";
+});
+
+triggerAll.addEventListener("click", () => {
+  changeColor.style.backgroundColor = "blue";
+  toggleColor.style.backgroundColor = "red";
+});
+
+triggerAllSelf.addEventListener("click", () => {
+  changeColor.style.backgroundColor = "blue";
+  toggleColor.style.backgroundColor = "red";
+  triggerAllSelf.style.backgroundColor = "green";
 });
